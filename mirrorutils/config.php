9a59f7c30c22e9c1ffd2cfcb011b9ed6944b0c1d
Tad <tad@spotco.us>
2023-02-06 13:58:24 -0500

    Initial move out of website repository
    Signed-off-by: Tad <tad@spotco.us>

---
+++ mirrorutils/config.php
@@ -0,0 +1,19 @@
+<?php
+//Copyright (c) 2017-2020 Divested Computing Group
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Lesser General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+$SBNR_DOMAIN_ALLOWLIST = array("divestos.org", "divestoseb5nncsydt7zzf5hrfg44md4bxqjs5ifcv4t7gt7u6ohjyyd.onion", "6sdlxbqgcxdbkvysoir2qvqqs5ro3fxgyl3phvuphcdyklv7rg57jhid.onion");
+
+?>
---
+++ mirrorutils/security.php
@@ -0,0 +1,30 @@
+<?php
+//Copyright (c) 2017-2020 Divested Computing Group
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Lesser General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//Disable some dangerous features
+ini_set("allow_url_fopen", "Off");
+ini_set("allow_url_include", "Off");
+ini_set("expose_php", "Off");
+ini_set("file_uploads", false);
+
+//Hardening
+ini_set("post_max_size", "2K");
+
+//Disable error/trace reporting
+ini_set("display_errors", "Off");
+error_reporting(E_ERROR | E_PARSE);
+
+?>
